9e6d9d12efdc5ed237cfd2670d78cc4057d937fc
NataliaPolak13 <272227@student.pwr.edu.pl>
2025-04-28 12:38:33 +0200

    vue.js added

---
+++ frontend_vue/src/components/formularz_rejestracyjny.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="form-wrapper">
+    <div class="form-container">
+      <div class="header">
+        <h2>Zarejestruj się</h2>
+      </div>
+      <form @submit.prevent="handleSubmit" class="form-content">
+        <div class="form-row">
+          <input v-model="firstName" type="text" placeholder="Imię *" required />
+          <input v-model="password" type="password" placeholder="Hasło *" required />
+        </div>
+        <div class="form-row">
+          <input v-model="lastName" type="text" placeholder="Nazwisko *" required />
+          <input v-model="confirmPassword" type="password" placeholder="Powtórz hasło *" required />
+        </div>
+        <div class="form-row">
+          <input v-model="email" type="email" placeholder="Email *" required />
+          <label class="upload-btn">
+            <span>📷 Załaduj zdjęcie profilowe</span>
+            <input type="file" @change="handleFileUpload" hidden />
+          </label>
+        </div>
+        <div class="form-submit">
+          <button type="submit">Zarejestruj się</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      profileImage: null
+    };
+  },
+  methods: {
+    handleSubmit() {
+      if (this.password !== this.confirmPassword) {
+        alert("Hasła nie pasują do siebie!");
+        return;
+      }
+
+      console.log("Rejestracja:", {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        password: this.password,
+        profileImage: this.profileImage
+      });
+
+      alert("Rejestracja zakończona sukcesem!");
+    },
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      if (file) {
+        this.profileImage = file;
+        console.log("Załadowano zdjęcie:", file);
+      }
+    }
+  }
+};
+</script>
+
+<style src="../assets/formularz_rejestracyjny.css"></style>
---
+++ frontend_vue/src/components/logowanie.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="login-container">
+    <div class="header">
+      <h2>Witaj ponownie</h2>
+    </div>
+    <div class="loging-bar">
+      <div class="login-form">
+        <div style="margin-bottom: 30px" class="input-group">
+          <span class="icon">📧</span>
+          <input v-model="email" type="email" placeholder="Email" />
+        </div>
+        <div class="input-group">
+          <span class="icon">🔒</span>
+          <input v-model="password" type="password" placeholder="Hasło" />
+        </div>
+      </div>
+      <div class="login-footer">
+        <button @click="handleSubmit">Zaloguj się</button>
+        <router-link to="/reset_hasła">Zapomniałeś hasła?</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    handleSubmit() {
+      // Możesz tu dodać logikę logowania
+      console.log("Email:", this.email);
+      console.log("Hasło:", this.password);
+    }
+  }
+};
+</script>
+
+<style src="../assets/logowanie.css"></style>
